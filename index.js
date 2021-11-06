5be909c2ae296591260b03b103f993529f9b6569
Ben <benbperry1@gmail.com>
2021-11-05 19:13:50 -0700

    commit tree c16d252bff58dd0dbbd6ccd8272a42ad9f241a4a

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+//1. Break array down into into individual words
+//2. Capitalize first letter of eveery word
+//3. rejoin strings together
+
+let titleCased = () => {
+  return tutorials.map((string) => {
+    const split = string.split(" ");
+    for (let i = 0; i < split.length; i++) {
+      split[i] = split[i][0].toUpperCase() + split[i].slice(1);
+    }
+
+    return split.join(" ");
+  });
+};
+
+console.log(titleCased());
+
+// const titleCased = tutorials.map((string) => {
+//     const split = string.split(" ")
+//     for (let i = 0; i < split.length; i++) {
+//       split[i]=split[i][0].toUpperCase()+split[i].slice(1)
+//     }
+
+//       return split.join(" ")
+
+// })
+
+// makes first character in string upperCase
+// return string.charAt(0).toUpperCase() + string.slice(1);
+
+// function titleCased(string) {
+//     string.split(" ")
+//     return string
+// }
+
+// return string.split(tutorials)
